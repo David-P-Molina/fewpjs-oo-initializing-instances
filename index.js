388e346df62087af5f6bdd3090037dd94bab083a
David Moina <davemolina777@gmail.com>
2021-05-27 12:55:10 -0500

    commit tree 3c7f0fb14454bb34b37ac6181320f964fb9d6668

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = dessert;
+    }
+}
+let newDinner = new Dinner("nice salad", "nice soup", "great entree", "dessert")
+console.log(newDinner)
+console.log(newDinner.dessert)
+console.log(newDinner.salad)
